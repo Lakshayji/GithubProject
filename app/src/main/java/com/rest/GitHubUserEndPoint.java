4dfe50c8e366c9e06fa515c269cc8b79ff422ef6
Lakshay <lakshaygirdher3@gmail.com>
2022-01-16 19:18:26 +0530

    first commit

---
+++ app/src/main/java/com/rest/GitHubUserEndPoint.java
@@ -0,0 +1,14 @@
+package com.rest;
+
+import com.model.GitHubUser;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GitHubUserEndPoint {
+     @GET("/users/{user}")
+     Call<GitHubUser> getUser(@Path("user") String user);
+
+
+}
